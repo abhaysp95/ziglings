daa1f3f2dfb76de74d73ad5c8bc011b45a804a2e
abhaysp95 <abhaysp95@protonmail.com>
2024-02-29 19:21:00 +0530

    feat: done till quiz5

--- exercises/044_quiz5.zig
+++ exercises/044_quiz5.zig
@@ -18,14 +18,19 @@ const Elephant = struct {
 
 pub fn main() void {
     var elephantA = Elephant{ .letter = 'A' };
-    // (Please add Elephant B here!)
+    var elephantB = Elephant{ .letter = 'B' };
     var elephantC = Elephant{ .letter = 'C' };
 
     // Link the elephants so that each tail "points" to the next elephant.
     // They make a circle: A->B->C->A...
     elephantA.tail = &elephantB;
     // (Please link Elephant B's tail to Elephant C here!)
+    elephantB.tail = &elephantC;
     elephantC.tail = &elephantA;
+
+    // while (!undefined) {  // I guess this is what will happen if you don't link elephantC to tail of elephantB
+    //     std.debug.print("This is true", .{});
+    // }
 
     visitElephants(&elephantA);
 
